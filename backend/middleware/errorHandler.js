9a1949902d73de32c129af053e4212ae5178839c
Avi Sinha <avisinha1711@gmail.com>
2025-11-13 12:01:46 +0530

    Add user and patient schemas with password handling

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -26,3 +26,52 @@ message: error.message || 'Server Error'
 });
 };
 module.exports = errorHandler;
+required: [true, 'Email is required'],
+unique: true,
+lowercase: true
+},
+password: {
+type: String,
+required: [true, 'Password is required'],
+minlength: [6, 'Password must be at least 6 characters'],
+select: false
+},
+role: {
+type: String,
+enum: ['researcher', 'doctor'],
+default: 'researcher'
+}
+}, {
+timestamps: true
+});
+// Encrypt password
+userSchema.pre('save', async function(next) {
+if (!this.isModified('password')) {
+next();
+}
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt);
+});
+// Match password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+return await bcrypt.compare(enteredPassword, this.password);
+};
+module.exports = mongoose.model('User', userSchema);
+unique: true
+},
+createdBy: {
+type: mongoose.Schema.Types.ObjectId,
+ref: 'User',
+required: true
+}
+}, {
+timestamps: true
+});
+// Generate research ID
+patientSchema.pre('save', function(next) {
+if (!this.researchId) {
+this.researchId = `RES${Date.now()}${Math.random().toString(36).substr(2, 5)}`.toUpperCase();
+}
+next();
+});
+module.exports = mongoose.model('Patient', patientSchema);
